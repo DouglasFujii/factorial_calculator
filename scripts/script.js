69604e6de2e0b6e33d768a0cee95d063a8b534c1
Douglas Fujii <doug.fujii@gmail.com>
2021-04-29 15:57:01 -0300

    handle with decimals and negative numbers

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,39 @@
-const inputFactorial = document.getElementById('factorial');
-
 function showResult() {
+    const inputFactorial = document.getElementById('factorial');
     const resultElement = document.getElementById('result');
     const description = document.createElement('p');
     const factResult = document.createElement('h1');
 
     resultElement.innerHTML = '';
 
-    description.innerHTML = 'O resultado de ' + inputFactorial.value + '! é:'
-    factResult.innerHTML = formatNumber(extraLongFactorials(inputFactorial.value));
-    resultElement.appendChild(description);
-    resultElement.appendChild(factResult);
+    if (inputFactorial.value < 0) {
+        description.innerHTML = 'Você digitou um número negativo! Fatoriais são calculados apenas com números inteiros e positivos. Tente digitar outro número.';
+        resultElement.appendChild(description);
+    } else if (inputFactorial.value.includes('.')) {
+        description.innerHTML = 'Você digitou um número decimal! Fatoriais são calculados apenas com números inteiros e positivos. Tente digitar outro número.'
+        resultElement.appendChild(description)
+    } else {
+        description.innerHTML = 'O resultado de ' + inputFactorial.value + '! é:';
+        factResult.innerHTML = formatNumber(extraLongFactorials(inputFactorial.value));
+        resultElement.appendChild(description);
+        resultElement.appendChild(factResult);
+    }
 }
 
 function formatNumber(num) {
     let res = BigInt(num);
     // The toLocaleString() method converts a number into a string, using a local language format.
-    return res.toLocaleString('pt-BR');    
+    return res.toLocaleString('pt-BR');
 }
 
 function extraLongFactorials(n) {
     // handle with big integers
     n = BigInt(n);
     let res = BigInt(1);
-    
-    for (let i = n; i > 0; i --) {
+
+    for (let i = n; i > 0; i--) {
         res *= i;
     }
-    
+
     return res.toString();
 }
